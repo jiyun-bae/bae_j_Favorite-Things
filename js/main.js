619e9fe05ea261549a9ec8ae36f356d7578df19d
Jiyun <baejiyun01@gmail.com>
2020-11-16 00:08:22 +0900

    Updating EVERYTHING

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// import your packages here
+import { fetchData } from "./modules/TheDataMiner.js";
+
+(() => {
+    // stub * just a place for non-component-specific stuff
+    console.log('loaded');
+    
+    function popErrorBox(message) {
+        alert("Something has gone horribly, horribly wrong");
+    }
+
+    function handleDataSet(data) {
+
+        let lightbox = document.querySelector(".lightbox");
+    }
+
+    function retrieveProjectInfo() {
+        // test for an ID
+        if (!event.target.id) { return }
+
+        fetchData(`./includes/index.php?id=${event.target.id}`).then(data => console.log(data)).catch(err => console.log(err));
+    }
+
+    function renderPortfolioThumbnails(thumbs) {
+        let userSection = document.querySelector('.user-section'),
+            userTemplate = document.querySelector('#user-template').content;
+
+        for (let user in thumbs) {
+            let currentUser = userTemplate.cloneNode(true),
+                currentUserText = currentUser.querySelector('.user').children;
+
+            currentUserText[1].src = `images/${thumbs[user].biopic}`;
+            currentUserText[1].id = thumbs[user].id;
+            // add this new user to the view
+            userSection.appendChild(currentUser);
+        }
+
+        userSection.addEventListener("click", retrieveProjectInfo);
+    }
+
+    fetchData("./includes/index.php").then(data => renderPortfolioThumbnails(data)).catch(err => console.log(err));
+
+})();
